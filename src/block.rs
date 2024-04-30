9acc7ff9cfb9698eeb34e645d8a6a3c1b52e3239
jayalwayscodes <johnsonjardocs@gmail.com>
2024-04-30 22:20:28 +0100

    created block.rs

---
+++ src/block.rs
@@ -0,0 +1,12 @@
+pub struct Block {
+    timestamp: u128,
+    transactions: String,
+    prev_block_hash: String,
+    hash: String,
+    height: usize,
+    nonce: i32,
+}
+
+pub struct Blockchain{
+    
+}
