1cb0b3a17af18a2df340897bbfad515e12873774
jayalwayscodes <johnsonjardocs@gmail.com>
2024-05-02 09:36:31 +0100

    edited and wrote tests

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -1,10 +1,11 @@
-use crate::lib::Block;
-use crate::errors::Result;
+use crate::error::Result;
+use crate::Block;
+use sled;
+use bincode;
+const TARGET_HEXT: usize = 4;
 
-
-
-#[derive(Debug)]
-pub struct Blockchain{
+#[derive(Debug, Clone)]
+pub struct Blockchain {
     current_hash: String,
     db: sled::Db,
 }
@@ -14,20 +15,19 @@ pub struct BlockchainIter<'a> {
     bc: &'a Blockchain,
 }
 
-
-impl Blockchain{
-    pub fn new() -> Result<Blockchain>{
+impl Blockchain {
+    pub fn new() -> Result<Blockchain> {
         let db = sled::open("data/blocks")?;
 
         match db.get("LAST")? {
             Some(hash) => {
                 let lasthash = String::from_utf8(hash.to_vec())?;
-                Ok(Blockchain{
+                Ok(Blockchain {
                     current_hash: lasthash,
                     db,
                 })
             }
-            None=> {
+            None => {
                 let block = Block::new_genesis_block();
                 db.insert(block.get_hash(), bincode::serialize(&block)?)?;
                 db.insert("LAST", block.get_hash().as_bytes())?;
@@ -39,24 +39,45 @@ impl Blockchain{
                 Ok(bc)
             }
         }
-
-        
     }
 
     pub fn add_block(&mut self, data: String) -> Result<()> {
         let lasthash = self.db.get("LAST")?.unwrap();
-        let new_block = Block::new_block(data, String::from_utf8(lasthash.to_vec())?, TARGET_HEXT )?;
-        self.db.insert(new_block.get_hash(), bincode::serialize(&new_block)?)?;
+        let new_block = Block::new_block(data, String::from_utf8(lasthash.to_vec())?, TARGET_HEXT)?;
+        self.db
+            .insert(new_block.get_hash(), bincode::serialize(&new_block)?)?;
         self.db.insert("LAST", new_block.get_hash().as_bytes())?;
         self.current_hash = new_block.get_hash();
         Ok(())
+    }
+
+    pub fn iter(&self) -> BlockchainIter {
+        BlockchainIter {
+            current_hash: self.current_hash.clone(),
+            bc: &self,
+        }
     }
 }
 
 impl<'a> Iterator for BlockchainIter<'a> {
     type Item = Block;
 
-    fn next(&mut self) -> Option<Self::Item>{
-        
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Ok(encode_block) = self.bc.db.get(&self.current_hash) {
+            return match encode_block {
+                Some(b) => {
+                    if let Ok(block) = bincode::deserialize::<Block>(&b) {
+                        self.current_hash = block.get_prev_hash();
+                        Some(block)
+                    } else {
+                        None
+                    }
+                }
+                None => None,
+            };
+        }
+        None
     }
 }
+
+
