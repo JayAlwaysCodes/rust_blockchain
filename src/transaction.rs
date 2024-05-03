3716d27ccc22e75aa897ca7a1d9110aa3a6e0b08
root <root@Jay.>
2024-05-03 21:55:27 +0100

    finished with transaction.rs

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,6 +1,8 @@
-use crate::error::Result;
+use crate::{blockchain::Blockchain, error::Result};
+use failure::format_err;
 use serde::{Deserialize, Serialize};
 use crypto::{digest::Digest, sha2::Sha256};
+use log::error;
 
 
 ///Transaction represents a Bitcoin transaction
@@ -32,6 +34,47 @@ pub struct TXOutput{
 }
 
 impl Transaction{
+    ///NewUTXOTransaction creates a new transaction
+    pub fn new_UTXO(from: &str, to: &str, amount: i32, bc: &Blockchain) -> Result<Transaction>{
+        let mut vin = Vec::new();
+        let acc_v = bc.find_spendable_outputs(from, amount);
+        if acc_v.0 < amount {
+            error!("Not enough balance");
+            return Err(format_err!("Not Enough balance: current balance {}", acc_v.0));
+        }
+
+        for tx in acc_v.1{
+            for out in tx.1{
+                let input = TXInput{
+                    txid: tx.0.clone(),
+                    vout: out,
+                    script_sig: String::from(from),
+                };
+                vin.push(input);
+            }
+        }
+
+        let mut vout = vec![TXOutput{
+            value: acc_v.0 - amount,
+            script_pub_key: String::from(to),
+        }];
+        if acc_v.0 > amount{
+            vout.push(TXOutput{
+                value: acc_v.0 - amount,
+                script_pub_key: String::from(from),
+            })
+        }
+
+        let mut tx = Transaction{
+            id: String::new(),
+            vin,
+            vout,
+        };
+        tx.set_id()?;
+
+        Ok(tx)
+    }
+
     pub fn new_coinbase(to: String, mut data: String) -> Result<Transaction> {
         if data == String::from(""){
             data += &format!("Reward to '{}' ", to);
